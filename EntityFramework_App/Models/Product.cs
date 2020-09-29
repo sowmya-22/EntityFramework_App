3f7a5f9127855d409f821dc9a38ca977e8c39466
SOWMYA KOTHWALGUDEM <sowmya.kothwalgudem@dxc.com>
2020-09-29 10:26:55 +0530

    Statshed in the StashedBranch

---
+++ EntityFramework_App/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework_App.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+    }
+}
